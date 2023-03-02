8a74a97eb4623171bc2c99a11073a87d1be0dd1e
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-03-02 16:01:00 +0200

    made task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let timerId = null;
 
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+console.log(startBtnRef.style);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function onStartBtnClick() {
+    timerId = setInterval(() => {
+        bodyRef.style.background = getRandomHexColor();
+        startBtnRef.setAttribute("disabled", "disabled");
+        stopBtnRef.removeAttribute("disabled", "disabled");
+    }, 1000)
+};
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtnRef.removeAttribute("disabled", "disabled");
+    stopBtnRef.setAttribute("disabled", "disabled");
+};
